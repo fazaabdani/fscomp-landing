266a1f957cc5807d4d083cb85c5447037000df15
fazaabdani <131626628+fazaabdani@users.noreply.github.com>
2026-05-21 11:27:10 +0700

    Tambah sitemap halaman utama

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://fscomp.id',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ];
+}
